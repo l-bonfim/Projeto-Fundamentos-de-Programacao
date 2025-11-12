7b63d831949fcbffe4c47fe7a218b892e9bfbe89
afws-bit <afws@cesar.school>
2025-11-12 17:18:51 -0300

    register.py alterado

--- backend/src/register.py
+++ backend/src/register.py
@@ -1,2 +1,21 @@
+from flask import request, jsonify
+
+saved_data = [{
+    'name': 'admin',
+    'id': 0,
+    'email': 'admin@admin',
+    'password': 'admin'
+}]
+
+def register_user():
+    data = request.get_json()
+    for i in range(len(saved_data)):
+        if data['name'] == saved_data[i]['name']:
+            return jsonify({
+                'message': 'user already exist'
+                'status' : 409
+            })
+    data['id'] = len(saved_data)
+    return data
 
 
